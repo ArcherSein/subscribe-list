52ae65be0ee5e87dde14ffc3016b0cea0cc16b5b
ArcherSein <35492630+ArcherSein@users.noreply.github.com>
2019-03-25 21:42:47 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+puppeteer.launch({
+    headless: true
+}).then(async (browser) => {
+    const page = await browser.newPage();
+    await page.goto('https://d.ishadowx.com/', {
+        waitUntil: 'networkidle0'
+    });
+    const url = await page.$eval('#portfolio > div.container > div:nth-child(2)', (ele) => {
+        const btns = ele.querySelectorAll('.v2 .copybtn');
+        const urlsv2 = Array.from(btns).map((btn) => {
+            return btn.dataset.clipboardText;
+        });
+        return btoa(urlsv2.join('\n'));
+    });
+    fs.writeFileSync('dist/urllist.txt', url);
+});
